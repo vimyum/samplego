eec96ef4ed0a5fd58506fe7df21436c41a514026
vimyum <1744970+vimyum@users.noreply.github.com>
2020-06-04 15:14:13 +0900

    Return http response

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/labstack/echo"
+	"net/http"
 )
 
+type response struct {
+	Message string `json:"message"`
+}
+
 func main() {
-	fmt.Printf("Hello World")
+	fmt.Printf("Start GO echo...")
+	resBody := response{Message: "hello"}
+
+	e := echo.New()
+	e.GET("/ping", func(c echo.Context) error {
+		//return c.String(http.StatusOK, "Hello, World!")
+		return c.JSON(http.StatusOK, resBody)
+	})
+	e.Logger.Fatal(e.Start(":8181"))
 }
