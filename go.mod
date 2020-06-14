bc6468f8e7a6a0adbebd9f6a7255c62020a8319f
vimyum <1744970+vimyum@users.noreply.github.com>
2020-06-15 00:03:48 +0900

    get github grass handler

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module sample
 go 1.14
 
 require (
+	github.com/google/uuid v1.1.1
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20200602180216-279210d13fed // indirect
--- main.go
+++ main.go
@@ -4,10 +4,29 @@ import (
 	"fmt"
 	"github.com/labstack/echo"
 	"net/http"
+	"os/exec"
+	"github.com/google/uuid"
 )
 
 type response struct {
 	Message string `json:"message"`
+}
+
+func MainPage() echo.HandlerFunc {
+    return func(c echo.Context) error {
+		username := c.Param("username")
+		u, _ := uuid.NewRandom()
+        uu := u.String()
+		tmpFileNmae := uu + ".svg"
+		cmdstr := fmt.Sprintf("curl https://github.com/%s | " + 
+			"awk '/<svg.+class=\"js-calendar-graph-svg\"/,/svg>/' | " +
+			"sed -e 's/<svg/<svg xmlns=\"http:\\/\\/www.w3.org\\/2000\\/svg\"/' > %s", username, tmpFileNmae);
+		cmd := exec.Command("sh", "-c", cmdstr)
+		cmd.Start()
+		cmd.Wait()
+
+        return c.File("./" + tmpFileNmae)
+    }
 }
 
 func main() {
@@ -19,5 +38,9 @@ func main() {
 		//return c.String(http.StatusOK, "Hello, World!")
 		return c.JSON(http.StatusOK, resBody)
 	})
+
+	e.GET("/grass/:username", MainPage())
+	
 	e.Logger.Fatal(e.Start(":8181"))
+
 }
